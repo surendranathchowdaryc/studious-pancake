15b4dfa7418d21ea177a0a37b5de0d1bb7948515
Surendranath Chowdaryc <surendranathchowdaryc@gmail.com>
2021-09-11 13:33:01 +0530

    spring initializer generated project

---
+++ actuator-and-swagger-app/src/main/java/com/example/springsamples/actuatorandswaggerapp/ActuatorAndSwaggerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsamples.actuatorandswaggerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorAndSwaggerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorAndSwaggerAppApplication.class, args);
+	}
+
+}
---
+++ actuator-and-swagger-app/src/test/java/com/example/springsamples/actuatorandswaggerapp/ActuatorAndSwaggerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsamples.actuatorandswaggerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorAndSwaggerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
