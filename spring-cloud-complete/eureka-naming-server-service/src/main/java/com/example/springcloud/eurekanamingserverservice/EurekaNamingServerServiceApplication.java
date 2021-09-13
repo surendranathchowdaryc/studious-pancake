2d62b0c358fe5fbcdb87969f659ac8b7bbe8227c
Surendranath Chowdaryc <surendranathchowdaryc@gmail.com>
2021-09-13 16:38:34 +0530

    comments added

--- spring-cloud-complete/eureka-naming-server-service/src/main/java/com/example/springcloud/eurekanamingserverservice/EurekaNamingServerServiceApplication.java
+++ spring-cloud-complete/eureka-naming-server-service/src/main/java/com/example/springcloud/eurekanamingserverservice/EurekaNamingServerServiceApplication.java
@@ -3,7 +3,9 @@ package com.example.springcloud.eurekanamingserverservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+/**
+ * MicroService for Service Registery otherwise called a naming server.
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaNamingServerServiceApplication {
